b0205f9b199c15afd162a8a4eafad51caa109b92
Karlis Lukstins <karlis.lukstins@gmail.com>
2015-05-18 15:07:58 +0300

    Various fixes, jslint, local 'window' and 'document', readable if conditions

--- js/infinite_scroll.js
+++ js/infinite_scroll.js
@@ -1,73 +1,81 @@
 "use strict";
+ var infiniteScroll = function($, window, document) {
+  return function (options) {
+    var is = this;
+    is.defaults = {
+      maxRows: 50,
+      threshold: 500,
+      selector: "#list",
+      rows: 0,
+      marginTop: 0
+    };
+    is.options = $.extend(is.defaults, options);
 
-var infiniteScroll = function(options){
-  var is = this;
-  is.defaults = {
-    maxRows: 50,
-    threshold: 500,
-    divId: "list",
-    rows: 0,
-    marginTop: 0
-  }
-  is.options = $.extend( is.defaults, options );
+    is.createItem = function (maxHeight) {
+      var height = Math.round(Math.random() * 200) + 100;
+      if (maxHeight < height) {height = maxHeight; } //Fix for random first element heigth
 
+      return $("<div>")
+        .addClass("item")
+        .css({"height": height,
+          "background-color": '#' + ('000000' + (Math.random()*0xFFFFFF<<0).toString(16)).slice(-6)
+          });
+    };
 
-  is.createItem = function (maxHeight) {
-    var height = Math.round(Math.random()*200) + 100;
-    if (maxHeight < height) height = maxHeight; //Fix for random first element heigth
+    is.prepend = function () {
+      var item = is.createItem(is.options.marginTop).prependTo(is.options.selector);
+      is.options.marginTop -= item.height();
+      $(is.options.selector).css({"margin-top": is.options.marginTop });
+    };
 
-    return $("<div>")
-      .addClass("item")
-      .css({"height": height,
-        "background-color": '#'+ ('000000' + (Math.random()*0xFFFFFF<<0).toString(16)).slice(-6)
-      });
-  }
+    is.append = function () {
+      if (is.options.rows < is.options.maxRows) {
 
-  is.prepend = function(){
-    var item = is.createItem(is.options.marginTop).prependTo("#" + is.options.divId);
-    $("#" + is.options.divId).css({"margin-top": is.options.marginTop -= item.height() });
-  }
+        var item = is.createItem().appendTo(is.options.selector);
+        this.options.rows++;
 
-  is.append = function(){
-    if (is.options.rows < is.options.maxRows){
-
-      var item = is.createItem().appendTo("#" + is.options.divId);
-      this.options.rows++;
-
-      if (is.options.rows == is.options.maxRows){
-        item.css({"border" : "15px dashed black"});
+        if (is.options.rows === is.options.maxRows) {
+          item.css({"border" : "15px dashed black"});
+        }
       }
-    }
+    };
+
+    is.scroll = function () {
+      var scrollTop = $(window).scrollTop();
+      var scrollBottom = scrollTop + $(window).height();
+      var bottomThreshold = $(document).height() - is.options.threshold;
+      var topThreshold = is.options.marginTop + is.options.threshold;
+
+      if (scrollBottom >= bottomThreshold) {
+        is.append();
+      } else if (is.options.marginTop !== 0 && scrollTop <= topThreshold) {
+        is.prepend();
+      }
+
+      $(".item").each(function () {
+        var $item = $(this);
+        var height = $item.height();
+        var itemPosition = $item.offset().top;
+        var itemBottomThreshold = itemPosition + is.options.threshold + height;
+        var itemTopThreshold = itemPosition - is.options.threshold;
+
+        if (scrollTop > itemBottomThreshold) {
+          is.options.marginTop += height;
+          $(is.options.selector).css({"margin-top": is.options.marginTop });
+          $item.remove();
+
+        } else if (scrollBottom < itemTopThreshold) {
+          $item.remove();
+          is.options.rows--;
+        }
+      });
+    };
+
+    // Load initial items
+    for (var i=0; i<10; ++i ){ is.append(); }
+
+    $(window).scroll(is.scroll);
+    return is;
   }
-
-  $(window).scroll(function(){
-    var windowHeight = $(window).height();
-    var scrollTop = $(window).scrollTop();
-
-    if ( scrollTop + windowHeight >= $(document).height() - is.options.threshold ){
-      is.append();
-    } else if ( is.options.marginTop != 0 && scrollTop - is.options.threshold <= is.options.marginTop ){
-      is.prepend();
-    }
-
-    $(".item").each(function() {
-      var item = $(this);
-      var height = item.height();
-      var itemPosition = item.offset().top;
-
-      if ( scrollTop > itemPosition + is.options.threshold + height  ){
-        is.options.marginTop += height;
-        item.remove();
-        $("#" + is.options.divId).css({"margin-top": is.options.marginTop});
-
-      } else if (itemPosition - is.options.threshold - height > scrollTop + windowHeight ){
-        item.remove();
-        is.options.rows--;
-      };
-    });
-  });
-
-  // Load initial items
-  for (var i=0; i<10; ++i ){ is.append(); }
-  return is;
-}
+  return infiniteScroll;
+}($, window, document);
